365024bb59ba341d5a20d110aeee14636e486859
Liders Jesus Toro Suarez <a2017115070@estudiantes.upsa.edu.bo>
2019-05-16 20:00:18 -0400

    con verificacion a internet  la api

---
+++ Xamarin/App/App/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+ 
+
+   public class Product
+   {
+       [Key]
+       public int ProductId { get; set; }
+
+       [Required]
+       [Display(Name = "Product Name")]
+       public string Name { get; set; }
+
+       public double Price { get; set; }
+   }
+}
